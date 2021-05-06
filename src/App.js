7595d46f22fcc2f29c4b41406e4afd1fe07845f1
nick sachau <nicksachau156@gmail.com>
2021-05-05 23:22:58 -0700

    messed with nav bar and styling a little bit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import React, { useState} from 'react'
 import CurrentDay from './components/CurrentDay';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <CurrentDay />
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 // import React, { useState } from 'react'
+import tempLogo from '../Images/tempLogo.png'
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <ul className="navbar">
+                <img src={tempLogo} alt=""/>
+                <input type="search" name="Search" id="" placeholder="Search"/>
+            </ul>
+        </nav>
     )
 }
 
