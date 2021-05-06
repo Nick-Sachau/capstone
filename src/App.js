ebea242d39195a11f2834c6759da15e4ca09abc1
Nick Sachau <nicksachau156@gmail.com>
2021-05-06 14:37:20 -0700

    pulling files

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import CurrentDay from './components/CurrentDay';
 import Navbar from './components/Navbar';
 import OtherDays from './components/OtherDays';
 import { Container } from 'react-bootstrap'
+import Weather from './weather.json'
 
 function App() {
+  let time = new Date(Weather.daily[0].dt)
+  console.log(time.getDay())
 
   return (
     <>
--- src/components/OtherDays.jsx
+++ src/components/OtherDays.jsx
@@ -1,11 +1,20 @@
 import { Container } from 'react-bootstrap'
 import Weather from '../weather.json'
+import { WiDegrees } from 'react-icons/wi'
 
 const OtherDays = () => {
+    console.log(Weather.daily)
+
     return (
         <Container className="nextDays">
-            <div className="icon"><img src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather icon"/></div>
-            <div className="Temp"><p>Temp: {Math.round(Weather.current.temp)}</p></div>
+            <div className="top">
+                <div className="Time">2 PM</div>
+                <div className="icon"><img src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather icon"/></div>
+            </div>
+            <div className="bottom">
+                <div className="TempMax"><p>{Math.round(Weather.daily[0].temp.max)}<WiDegrees className="deg" /></p></div>
+                <div className="TempMin"><p>{Math.round(Weather.daily[0].temp.min)}<WiDegrees className="deg" /></p></div>
+            </div>
         </Container>
     )
 }
