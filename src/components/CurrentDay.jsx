77360f043c2b590cc2d7f302e49de8ecc3b4d45b
Adrian Sapien <asapie269@west-mec.org>
2021-05-11 15:11:44 -0700

    dum

--- src/components/CurrentDay.jsx
+++ src/components/CurrentDay.jsx
@@ -9,9 +9,10 @@ import { AiFillCaretUp } from 'react-icons/ai'
 import { Button } from 'react-bootstrap'
 
 
-const CurrentDay = ({}) => {
+const CurrentDay = ({ information }) => {
 
-    // const [active, setActive] = useState(false)
+    console.log(information)
+
     const [open, setOpen] = useState(false)
 
     return (
@@ -19,9 +20,9 @@ const CurrentDay = ({}) => {
             <Jumbotron>
                 <div className="details">
                     <div className="date">
-                        <div className="day">{info.dt}</div>
-                        <div className="weekDay"></div>
-                        <div className="year"></div>
+                        <div className="day">{information.dt}</div>
+                        <div className="weekDay">{information.month} {information.day}</div>
+                        <div className="year">{information.year}</div>
                     </div>
                     <div className="weather">
                         <div className="temp">{Math.round(information.temp.day)}<WiDegrees className="deg" /></div>
