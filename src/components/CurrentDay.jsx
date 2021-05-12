80c7551a1cb4802c0b31c3de90185d9b140e31b4
david martin <dmartin8624@gmail.com>
2021-05-12 13:07:07 -0700

    last little something that i though it pushed yesterday but evidently didnt

--- src/components/CurrentDay.jsx
+++ src/components/CurrentDay.jsx
@@ -26,30 +26,30 @@ const CurrentDay = ({ information }) => {
                     </div>
                     <div className="weather">
                         <div className="temp">{Math.round(information.temp.day)}<WiDegrees className="deg" /></div>
-                        <div className="weather"><img className="icon" src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather icon"/> </div>
+                        <div className="weather"><img className="icon" src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather icon" /> </div>
                         <div className="humidity">Humidity: {information.humidity}%</div>
                     </div>
                 </div>
                 <div className="content">
                     <p>This is filler content. Deserunt magna pariatur non nulla culpa qui irure elit aliqua mollit voluptate nulla officia fugiat. Non esse irure ad aute cupidatat officia ad sint quis aliqua pariatur quis dolore. Sunt velit veniam consectetur labore consequat laboris.</p>
                 </div>
-                
+
             </Jumbotron>
             <Collapse in={open}>
-                    <div id="collapse-text">
-                        This is filler text, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad placeat nostrum reiciendis optio aliquam culpa sequi maiores veritatis aperiam reprehenderit impedit dolores corrupti distinctio, error molestias amet provident enim labore?
+                <div id="collapse-text">
+                    This is filler text, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad placeat nostrum reiciendis optio aliquam culpa sequi maiores veritatis aperiam reprehenderit impedit dolores corrupti distinctio, error molestias amet provident enim labore?
                         <br></br>
-                        <br></br>
-                        
-                    </div>
-                    
-                </Collapse>
-            
+                    <br></br>
+
+                </div>
+
+            </Collapse>
+
             <Button
-            onClick={() => setOpen(!open)}
-            aria-controls="example-collapse=text"
-            aria-expanded={open}
-            className="collapseButton"
+                onClick={() => setOpen(!open)}
+                aria-controls="example-collapse=text"
+                aria-expanded={open}
+                className="collapseButton"
             >{!open ? <AiFillCaretDown /> : <AiFillCaretUp />}</Button>
         </>
     )
